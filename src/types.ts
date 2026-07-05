9b58a5ac30462050ab5ae18e2b317b7909908392
Timo Haas <timo-42@users.noreply.github.com>
2026-07-06 00:43:55 +0200

    Parse ECMA-119 descriptor sequences

--- src/types.ts
+++ src/types.ts
@@ -43,15 +43,27 @@ export type IsoDirectoryEntry = {
 
 export type IsoNode = IsoFileEntry | IsoDirectoryEntry;
 
-export type VolumeDescriptor = {
+export type BaseVolumeDescriptor = {
   type: number;
+  kind: string;
   identifier: string;
   version: number;
   offset: number;
+  sector: number;
+  raw: Uint8Array;
 };
 
-export type PrimaryVolumeDescriptor = VolumeDescriptor & {
+export type BootVolumeDescriptor = BaseVolumeDescriptor & {
+  type: 0;
+  kind: "boot";
+  bootSystemIdentifier: string;
+  bootIdentifier: string;
+  bootSystemUse: Uint8Array;
+};
+
+export type PrimaryVolumeDescriptor = BaseVolumeDescriptor & {
   type: 1;
+  kind: "primary";
   systemIdentifier: string;
   volumeIdentifier: string;
   volumeSpaceSize: number;
@@ -72,7 +84,56 @@ export type PrimaryVolumeDescriptor = VolumeDescriptor & {
   effectiveAt: Date | null;
 };
 
+export type SupplementaryVolumeDescriptor = BaseVolumeDescriptor & {
+  type: 2;
+  kind: "supplementary";
+  version: 1;
+  volumeFlags: number;
+  systemIdentifier: string;
+  volumeIdentifier: string;
+  escapeSequences: Uint8Array;
+};
+
+export type EnhancedVolumeDescriptor = BaseVolumeDescriptor & {
+  type: 2;
+  kind: "enhanced";
+  version: 2;
+  volumeFlags: number;
+  systemIdentifier: string;
+  volumeIdentifier: string;
+  escapeSequences: Uint8Array;
+};
+
+export type VolumePartitionDescriptor = BaseVolumeDescriptor & {
+  type: 3;
+  kind: "partition";
+  systemIdentifier: string;
+  volumePartitionIdentifier: string;
+  volumePartitionLocation: number;
+  volumePartitionSize: number;
+  systemUse: Uint8Array;
+};
+
+export type VolumeDescriptorSetTerminator = BaseVolumeDescriptor & {
+  type: 255;
+  kind: "terminator";
+};
+
+export type UnknownVolumeDescriptor = BaseVolumeDescriptor & {
+  kind: "unknown";
+};
+
+export type VolumeDescriptor =
+  | BootVolumeDescriptor
+  | PrimaryVolumeDescriptor
+  | SupplementaryVolumeDescriptor
+  | EnhancedVolumeDescriptor
+  | VolumePartitionDescriptor
+  | VolumeDescriptorSetTerminator
+  | UnknownVolumeDescriptor;
+
 export type IsoImage = {
+  descriptors: VolumeDescriptor[];
   primaryVolumeDescriptor: PrimaryVolumeDescriptor;
   files: IsoFileEntry[];
   root: IsoDirectoryEntry;
